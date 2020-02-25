8a80e13c737cdf2461ddd2d699fbcf8caabecb24
vaie <vaielx@163.com>
2020-02-26 00:16:16 +0800

    Initial commit

---
+++ src/main/java/com/outdd/api/AwmApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.outdd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwmApiServerApplication.class, args);
+    }
+
+}
