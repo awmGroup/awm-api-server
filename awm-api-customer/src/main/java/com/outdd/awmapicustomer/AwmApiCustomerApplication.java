3f9e4a0fadce8a7023bb28ed0076308f4af3e227
vaie <vaielx@163.com>
2020-02-29 12:51:17 +0800

    init

---
+++ awm-api-customer/src/main/java/com/outdd/awmapicustomer/AwmApiCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.outdd.awmapicustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwmApiCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwmApiCustomerApplication.class, args);
+    }
+
+}
