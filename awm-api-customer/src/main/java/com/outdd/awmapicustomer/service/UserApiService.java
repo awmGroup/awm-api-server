6da5f41c90b2165cc6f43ffc8bc9c57858049384
vaie <vaielx@163.com>
2020-03-29 19:53:24 +0800

    Schema

--- awm-api-customer/src/main/java/com/outdd/awmapicustomer/service/UserApiService.java
+++ awm-api-customer/src/main/java/com/outdd/awmapicustomer/service/UserApiService.java
@@ -1,8 +1,6 @@
 package com.outdd.awmapicustomer.service;
 
 import com.outdd.awmapicustomer.pojo.User;
-import com.outdd.common.PageHelp;
-import com.outdd.common.base.ResponseBase;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,15 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public interface UserApiService {
 
-    @RequestMapping("/findById/{id}")
-    public ResponseBase findById(@PathVariable(value = "id") String id);
-
-    @RequestMapping("findUser/{username}/{password}")
-    public ResponseBase findByUsernameAndPassword(@PathVariable("username") String username, @PathVariable("password") String password);
-
     @RequestMapping("loadUserByUsername/{username}")
     public User loadUserByUsername(@PathVariable("username") String username);
 
-    @RequestMapping("findUsers")
-    public ResponseBase findUsers(@PathVariable("page") PageHelp page, @PathVariable("entity") User entity);
 }
